b94c15845c7d59d3e1b823be5b542d0753c4880a
hangphan <hangphan@well.ox.ac.uk>
2017-06-12 14:41:53 +0000

    changes to downloadSRA script

--- src/downloadSRA.sh
+++ src/downloadSRA.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-#$ -cwd
-#$ -V
 
 f=$1
-if [ ! -s ${f}_1.fq ]
+if [ ! -s ${f}_1.fastq.gz ]
 then
     fastq-dump --origfmt -I --split-files $f    --gzip 
 fi
 
-(mkdir -p fastq/$f
-mv ${f}_1.fastq.gz fastq/$f/reads1.fq.gz
-mv ${f}_2.fastq.gz fastq/$f/reads2.fq.gz) &
